c07af5381f38c50fb3c81c6e21400f71eae890f9
APareja12 <alexis.pareja@hotmail.com>
2022-08-12 09:38:40 -0400

    creates circle

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ canvas.height = window.innerHeight;
 
 
 
-canvas.addEventListener('click', function (e) {
+// canvas.addEventListener('click', function (e) {
     
-ctx.beginPath();
-ctx.rect(e.x, e.y, 10, 10);
-ctx.fill();
-});
+// ctx.beginPath();
+// ctx.rect(e.x, e.y, 10, 10);
+// ctx.fill();
+// });
 
 // const degToRad = (deg) =>{
 //     return deg /180 * Math.PI;
 // }
 
-// ctx.beginPath();
-// ctx.arc(100, 100, 50, 100, degToRad(180));
-// ctx.stroke();
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, 100);
+ctx.fill();
 
 
