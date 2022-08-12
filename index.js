865dd05e067b4bdabca44effa0946ef5c8c8fb7d
APareja12 <alexis.pareja@hotmail.com>
2022-08-12 09:52:21 -0400

    generates atoms

--- index.js
+++ index.js
@@ -12,12 +12,69 @@ canvas.height = window.innerHeight;
 // ctx.fill();
 // });
 
-const degToRad = (deg) =>{
-    return deg /180 * Math.PI;
+// const degToRad = (deg) =>{
+//     return deg /180 * Math.PI;
+// }
+
+// ctx.beginPath();
+// ctx.arc(100, 100, 50, 0, degToRad(180));
+// ctx.fill();
+
+let atoms = [];
+
+
+canvas.addEventListener('click', function(e){
+    for (let i = 0; i < 20; i++) {
+       atoms.push(new Atom(e.x, e.y));
+        // console.log("hello");
+    }
+});
+
+const animate = () => {
+    atoms.forEach(atom => {
+    // ctx.fillStyle = 'white'
+        atom.draw();
+        atom.update();
+        // atom.updateSpeed(); 
+        // atom.updateSize(); 
+
+        // if (atom.radius < 0.3){
+        //     atoms.splice(index, 1);
+        
+});
+    // ctx.save();
+    // ctx.fillStyle = 'rgba(255,255,255,0.3';
+    // ctx.fillStyle = 'rgba(0,0,0,0.2';
+    // ctx.fillRect(0,0,canvas.width, canvas.height);
+    // ctx.restore();
+    requestAnimationFrame(animate);
 }
 
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, degToRad(180));
-ctx.fill();
 
+animate();
 
+class Atom {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.radius = Math.random() * 8 + 2;
+        this.speedX = Math.random() * 4 - 2;
+        this.speedY = Math.random() * 4 - 2;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    // updateSize(){
+    //     this.radius -= 0.1;
+    //     // this.x += this.speedX;
+    //     // this.y += this.speedY;
+    // }
+
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2)
+        ctx.fill();
+    }
+}
