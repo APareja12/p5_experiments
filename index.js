d7be9f407cd3a67a1916417009993c9f63abffea
APareja12 <alexis.pareja@hotmail.com>
2022-08-12 09:31:08 -0400

    stroke square

---
+++ index.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+
+// canvas.addEventListener('click', function (e) {
+    
+ctx.beginPath();
+ctx.rect(100, 100, 100, 100);
+ctx.stroke();
+// });
+
+// const degToRad = (deg) =>{
+//     return deg /180 * Math.PI;
+// }
+
+ctx.beginPath();
+ctx.arc(100, 100, 50, 100, degToRad(180));
+ctx.stroke();
+
+
